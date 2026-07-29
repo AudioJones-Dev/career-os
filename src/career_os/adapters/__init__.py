35faab7fd3276c1441fcb8216476fa742f3f8f81
AudioJones <166316865+AudioJones-Dev@users.noreply.github.com>
2026-07-29 17:40:49 -0400

    Add ATS adapter package

---
+++ src/career_os/adapters/__init__.py
@@ -0,0 +1,9 @@
+from .greenhouse import fetch_greenhouse_jobs, normalize_greenhouse_job
+from .lever import fetch_lever_jobs, normalize_lever_job
+
+__all__ = [
+    "fetch_greenhouse_jobs",
+    "normalize_greenhouse_job",
+    "fetch_lever_jobs",
+    "normalize_lever_job",
+]
